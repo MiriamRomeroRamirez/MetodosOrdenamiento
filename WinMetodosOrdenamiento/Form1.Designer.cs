edd3089eba427aba2492557225d99e7aae938dcd
MiriamRomeroRamirez <71841430+MiriamRomeroRamirez@users.noreply.github.com>
2022-12-19 16:33:06 -0600

    Aplicación terminada

---
+++ WinMetodosOrdenamiento/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace WinMetodosOrdenamiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar archivo XML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(185, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(571, 263);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(60, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Visualizar archivo XML";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(116, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 173);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(20, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "ordenar numeros";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(20, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 35);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "ordenar cadenas";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Location = new System.Drawing.Point(185, 312);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(251, 199);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Metodo burbuja";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.listBox2);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Location = new System.Drawing.Point(505, 309);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(251, 199);
+            this.panel2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Metodo shellsort";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(20, 26);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 35);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "ordenar numeros";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(116, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 173);
+            this.listBox2.TabIndex = 3;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(20, 67);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 35);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "ordenar cadenas";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 542);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WinMetodosOrdenamiento/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WinMetodosOrdenamiento
+{
+    public partial class Form1 : Form
+    {
+        DataSet contenedor = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            Thread mihilo = new Thread(
+                    delegate()
+                    {
+                        CargarArchivo();
+                        if (button1.InvokeRequired)
+                        {
+                            button1.Invoke(new MethodInvoker(delegate
+                            {
+                                button1.Enabled = true;
+                            }));
+                        }
+                    }
+                );
+            mihilo.Start();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            Thread mihilo = new Thread(
+                    delegate ()
+                    {
+                        
+                        if (dataGridView1.InvokeRequired)
+                        {
+                            dataGridView1.Invoke(new MethodInvoker(delegate
+                            {
+                                MostrarArchivo();
+                            }));
+                        }
+                        if (button2.InvokeRequired)
+                        {
+                            button2.Invoke(new MethodInvoker(delegate
+                            {
+                                button2.Enabled = true;
+                            }));
+                        }
+                    }
+                );
+            mihilo.Start();
+        }
+        public void CargarArchivo()
+        {
+            contenedor.ReadXml(@"E:\PWA\datos.xml");
+            MessageBox.Show("Datos cargados");
+        }
+
+        public void MostrarArchivo()
+        {
+            dataGridView1.DataSource = contenedor.Tables[0];
+            MessageBox.Show("Tiene " + contenedor.Tables[0].Rows.Count + " total de registros");
+        }
+
+
+        public void ordenamientoBurbujaEnteros()
+        {
+            int[] numeros = new int[20];
+            Random aleatorio = new Random();
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = aleatorio.Next(50, 200) + 1;
+            }
+            int x = 0, indiceInicial = 0, indiceSiguiente = 0, temporal = 0;
+            for (x = 0; x < numeros.Length; x++)
+            {
+                for (indiceInicial = 0; indiceInicial < numeros.Length - 1; indiceInicial++)
+                {
+                    indiceSiguiente = indiceInicial + 1;
+                    if(numeros[indiceInicial] > numeros[indiceSiguiente])
+                    {
+                        temporal = numeros[indiceInicial];
+                        numeros[indiceInicial] = numeros[indiceSiguiente];
+                        numeros[indiceSiguiente] = temporal;
+                    }
+                }
+            }
+            int c = 0;
+            listBox1.Items.Clear();
+            for (c = 0; c < numeros.Length; c++)
+            {
+                listBox1.Items.Add(numeros[c]);
+            }
+        }
+        public void ordenamientoBurbujaCadena()
+        {
+            DataTable cadenas_ = contenedor.Tables[0];
+            int i = 0, IndiceInicial = 0;
+            for (i = 0; i < cadenas_.Rows.Count; i++)
+            {
+                for (IndiceInicial = 0; IndiceInicial < cadenas_.Rows.Count - 1; IndiceInicial++)
+                {
+                    int indiceProxima = IndiceInicial + 1;
+                    if ((cadenas_.Rows[IndiceInicial]["descripcion"].ToString().Trim()).CompareTo(cadenas_.Rows[indiceProxima]["descripcion"].ToString().Trim()) > 0)
+                    {
+                        string descrip = cadenas_.Rows[IndiceInicial]["descripcion"].ToString().Trim();
+                        string sat = cadenas_.Rows[IndiceInicial]["sat_unimed"].ToString();
+                        string simb = cadenas_.Rows[IndiceInicial]["simbolo"].ToString();
+                        cadenas_.Rows[IndiceInicial]["descripcion"] = cadenas_.Rows[indiceProxima]["descripcion"];
+                        cadenas_.Rows[IndiceInicial]["sat_unimed"] = cadenas_.Rows[indiceProxima]["sat_unimed"];
+                        cadenas_.Rows[IndiceInicial]["simbolo"] = cadenas_.Rows[indiceProxima]["simbolo"];
+                        cadenas_.Rows[indiceProxima]["descripcion"] = descrip;
+                        cadenas_.Rows[indiceProxima]["sat_unimed"] = sat;
+                        cadenas_.Rows[indiceProxima]["simbolo"] = simb;
+                    }
+                }
+            }
+        }
+
+        public void ordenamientoShellSort()
+        {
+            DataTable cadenas = contenedor.Tables[0];
+            int salto = 0;
+            int sw = 0;
+            int a = 0;
+            salto = cadenas.Rows.Count / 2;
+            while (salto > 0)
+            {
+                sw = 1;
+                while (sw != 0)
+                {
+                    sw = 0;
+                    a = 1;
+                    while (a <= (cadenas.Rows.Count - salto))
+                    {
+                        if ((cadenas.Rows[a - 1]["descripcion"].ToString().Trim()).CompareTo((cadenas.Rows[(a - 1) + salto]["descripcion"].ToString().Trim())) > 0)
+                        {
+                            string descrip = cadenas.Rows[(a - 1) + salto]["descripcion"].ToString().Trim();
+                            string sat = cadenas.Rows[(a - 1) + salto]["sat_unimed"].ToString();
+                            string simb = cadenas.Rows[(a - 1) + salto]["simbolo"].ToString();
+
+                            cadenas.Rows[(a - 1) + salto]["descripcion"] = cadenas.Rows[a - 1]["descripcion"].ToString();
+                            cadenas.Rows[(a - 1) + salto]["sat_unimed"] = cadenas.Rows[a - 1]["sat_unimed"].ToString();
+                            cadenas.Rows[(a - 1) + salto]["simbolo"] = cadenas.Rows[a - 1]["simbolo"].ToString();
+                            cadenas.Rows[(a - 1)]["descripcion"] = descrip;
+                            cadenas.Rows[(a - 1)]["sat_unimed"] = sat;
+                            cadenas.Rows[(a - 1)]["simbolo"] = simb;
+
+                            sw = sw - 1;
+                        }
+                        a++;
+                    }
+                }
+                salto = salto / 2;
+            }
+        }
+
+        public void ordenamientoEnterosShellSort()
+        {
+            int[] numeros = new int[20];
+            Random aleatorio = new Random();
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = aleatorio.Next(50, 200) + 1;
+            }
+
+            int salto = 0;
+            int sw = 0;
+            int a = 0;
+            salto = numeros.Length / 2;
+            while (salto > 0)
+            {
+                sw = 1;
+                while (sw != 0)
+                {
+                    sw = 0;
+                    a = 1;
+                    while (a <= (numeros.Length - salto))
+                    {
+                        if (numeros[a - 1] > numeros[a - 1 + salto])
+                        {
+                            int temp = numeros[(a - 1) + salto];
+                            numeros[(a - 1) + salto] = numeros[a - 1];
+                            numeros[a - 1] = temp;
+                            sw = sw - 1;
+                        }
+                        a++;
+                    }
+                }
+                salto = salto / 2;
+            }
+
+            int c = 0;
+            listBox2.Items.Clear();
+            for (c = 0; c < numeros.Length; c++)
+            {
+                listBox2.Items.Add(numeros[c]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread mihilo = new Thread(
+                    delegate ()
+                    {
+                        if (listBox1.InvokeRequired)
+                        {
+                            listBox1.Invoke(new MethodInvoker(delegate
+                            {
+                                ordenamientoBurbujaEnteros();
+                            }));
+                        }
+                    }
+                    );
+                    mihilo.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = false;
+            button6.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            Thread mihilo = new Thread(
+                    delegate ()
+                    {
+                        ordenamientoBurbujaCadena();
+                        if (dataGridView1.InvokeRequired)
+                        {
+                            dataGridView1.Invoke(new MethodInvoker(delegate {
+                                MessageBox.Show("Proceso terminado");
+                            }));
+                        }
+                        if (button1.InvokeRequired)
+                        {
+                            button1.Invoke(new MethodInvoker(delegate
+                            {
+                                button1.Enabled = true;
+                            }));
+                        }
+                        if (button2.InvokeRequired)
+                        {
+                            button2.Invoke(new MethodInvoker(delegate
+                            {
+                                button2.Enabled = true;
+                            }));
+                        }
+                        if (button4.InvokeRequired)
+                        {
+                            button4.Invoke(new MethodInvoker(delegate
+                            {
+                                button4.Enabled = true;
+                            }));
+                        }
+                        if (button6.InvokeRequired)
+                        {
+                            button6.Invoke(new MethodInvoker(delegate
+                            {
+                                button6.Enabled = true;
+                            }));
+                        }
+                    }
+                );
+            mihilo.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Thread mihilo = new Thread(
+                    delegate ()
+                    {
+                        if (listBox1.InvokeRequired)
+                        {
+                            listBox1.Invoke(new MethodInvoker(delegate
+                            {
+                                ordenamientoEnterosShellSort();
+                            }));
+                        }
+                    }
+                    );
+            mihilo.Start();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button6.Enabled = false;
+            button4.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            Thread mihilo = new Thread(
+                    delegate ()
+                    {
+                        ordenamientoShellSort();
+                        if (dataGridView1.InvokeRequired)
+                        {
+                            dataGridView1.Invoke(new MethodInvoker(delegate {
+                                MessageBox.Show("Proceso terminado");
+                            }));
+                        }
+                        if (button1.InvokeRequired)
+                        {
+                            button1.Invoke(new MethodInvoker(delegate
+                            {
+                                button1.Enabled = true;
+                            }));
+                        }
+                        if (button2.InvokeRequired)
+                        {
+                            button2.Invoke(new MethodInvoker(delegate
+                            {
+                                button2.Enabled = true;
+                            }));
+                        }
+                        if (button6.InvokeRequired)
+                        {
+                            button6.Invoke(new MethodInvoker(delegate
+                            {
+                                button6.Enabled = true;
+                            }));
+                        }
+
+                        if (button4.InvokeRequired)
+                        {
+                            button4.Invoke(new MethodInvoker(delegate
+                            {
+                                button4.Enabled = true;
+                            }));
+                        }
+                    }
+                );
+            mihilo.Start();
+
+
+        }
+    }
+}
